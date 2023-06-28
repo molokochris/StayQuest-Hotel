57b65cd4010c1a12c79cc1d1fd3c55a9f13314cf
molokochris <molokochrisp742@gmail.com>
2023-06-28 15:32:07 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Home from './Pages/Home'
 import Facilities from './Components/Facilities'
 import About from './Components/About'
 import Contact from './Components/Contact'
-// import Dining from './Components/Dining'
-// import { Route, Routes, Link } from "react-router-dom";
 
 
 function App() {
