254888f1f2ccfe1d1b976f1fbbd0a0676711a395
molokochris <molokochrisp742@gmail.com>
2023-07-05 21:52:03 +0200

    fix modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -8,7 +8,15 @@ export default function Modal({openModal, setOpenModal}) {
         <div className='head'>StayQuest</div>
         <div className='s-head'>Standard Room</div>
         <div className='i-head'></div>
-        <button onClick={() => setOpenModal(!openModal)}>close</button>
+        <div className="z-section">
+          <div className='rm-facilities'>facilities</div>
+          <div className="rm-description">info</div>
+          <div className='rm-price'>
+            <div className='rm-price-brk'>Price breakdown</div>
+            <div className='rm-pay'>pay</div>
+          </div>
+        </div>
+        <button className='close-btn' onClick={() => setOpenModal(!openModal)}>X</button>
     </div>
   )
 }
