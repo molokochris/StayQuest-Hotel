e069dba7302a7692792dfab2a7635d1c81de2498
molokochris <molokochrisp742@gmail.com>
2023-07-05 15:35:13 +0200

    modal content

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -6,6 +6,8 @@ export default function Modal({openModal, setOpenModal}) {
   return (
     <div className='modal-container'>
         <div className='head'>StayQuest</div>
+        <div className='s-head'>Standard Room</div>
+        <div className='i-head'></div>
         <button onClick={() => setOpenModal(!openModal)}>close</button>
     </div>
   )
